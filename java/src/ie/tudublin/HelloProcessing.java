3a275ff6e7ccbf9fbb5cebbb9018a1735d9d05c3
DimaKosy <dmytrokosynskyy@gmail.com>
2023-01-31 22:43:00 +0000

    drawing all seeing eye

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -11,22 +11,31 @@ public class HelloProcessing extends PApplet
 	}
 
 	public void setup() {
-		background(0);
-
+		background(255,0,0);
 		smooth();
 	}
 	int value;
 	
 	public void draw() {
-		fill(255*value,(255*value),0);
-		rect(25, 25, 50, 50);
+		noStroke();
+		fill(255,255,0);
+		circle(width/2,height/2+30,width-30);
+		fill(0,255,255);
+		triangle(width/2, 0, 30, height-30, width-30, height-30);
+		fill(100);
+		ellipse(width/2, height/2 -30, 200, 100);
+		fill(0);
+		circle(width/2, height/2 -30,90);
+
+		int distance = 130;
+
+		if(mouseX < width/2 + distance && mouseX > width/2 - distance && mouseY < width/2 + distance && mouseY > height/2 - distance){
+			stroke(0,90,90);
+			strokeWeight(2);
+			fill(0,130,130);
+			ellipse(width/2, height/2 -30, 200, 100);
+		}
+		
 	}
 	
-	public void keyPressed() {
-		if (value == 0) {
-			value = 1;
-		} else {
-			value = 0;
-		}
-	}
 }
