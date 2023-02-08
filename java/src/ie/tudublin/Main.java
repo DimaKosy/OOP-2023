389647743b0632bc8ea5e94bc1ce42d781c792af
DimaKosy <dmytrokosynskyy@gmail.com>
2023-02-08 22:22:38 +0000

    Circle boi

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -2,23 +2,10 @@ package ie.tudublin;
 
 public class Main
 {
-
 	public static void helloProcessing()
 	{
 		String[] a = {"MAIN"};
         processing.core.PApplet.runSketch( a, new HelloProcessing());
-    }
-
-	public static void loops()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Loops());
-    }
-
-	public static void bugZap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new BugZap());
     }
 	
 	public static void main(String[] args)
@@ -29,7 +16,7 @@ public class Main
 		penny.setName("Penny");
 		penny.speak();
 
-		loops();
+		helloProcessing();
 	}
 	
 }
