c3162478b16cfe219da65f10a23e1813d8aa41c4
DimaKosy <dmytrokosynskyy@gmail.com>
2023-02-13 14:44:24 +0000

    starmap

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -10,16 +10,32 @@ public class StarMap extends PApplet
 	}
 
 	public void setup() {
-		colorMode(HSB);
+		colorMode(RGB);
 		background(0);
+		textAlign(CENTER);
 
 		smooth();
+		noLoop();
 		
 	}
 
 		
 	public void draw()
 	{	
-		strokeWeight(2);		
+		strokeWeight(2);
+		stroke(0,255,0);
+
+		drawGrid( 50 , width,10);	
 	}
+
+	void drawGrid(float border, float size, float amount){
+		for(float i = 0; i <= amount; i += 1){
+			float displace = border + i * ((size - 2*border)/(amount));
+			line(border,displace, size - border, displace);
+			line(displace, border,displace, size - border);
+			
+			text(str((int)(i - amount/2)),displace,border/2);
+			text(str((int)(i - amount/2)),border/2,displace);
+		}
+	} 
 }
