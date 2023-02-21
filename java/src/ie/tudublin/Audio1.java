2804299d78c65ca2b13ae2546abfeb524f887618
DimaKosy <dmytrokosynskyy@gmail.com>
2023-02-21 12:24:47 +0000

    audio

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -11,7 +11,7 @@ public class Audio1 extends PApplet{
     Minim minim;
     AudioInput Ai;
     AudioPlayer Ap;
-    AudioBuffer Ab;
+    AudioBuffer Ab, lerpBuffer;
 
 	int FrameSize = 1024;
 	float Avg = 0;
