37a772a1babb8d3f5db2e649e67253e14a8032a4
DimaKosy <dmytrokosynskyy@gmail.com>
2023-03-01 16:26:32 +0000

    birds & flocks

---
+++ java/src/ie/tudublin/Birds.java
@@ -0,0 +1,18 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class Birds extends PApplet{
+    public void settings(){
+        size(600,600);
+    }
+
+    public void setup(){
+        
+    }
+
+    public void draw(){
+        background(0);
+
+    }
+}
